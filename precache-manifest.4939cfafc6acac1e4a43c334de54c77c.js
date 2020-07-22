b9afb279a7101c9360a763e28b5e829fd731d852
Jeeson Domiguez <dominguez_js@outlook.com>
2020-07-21 22:30:47 -0600

    Updates

---
+++ precache-manifest.4939cfafc6acac1e4a43c334de54c77c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df40d39443eac1db0f3b041f724da4b",
+    "url": "/react-BreakingBadQuotes/index.html"
+  },
+  {
+    "revision": "e40e2564d498a6e6b5c5",
+    "url": "/react-BreakingBadQuotes/static/css/main.b8176ca4.chunk.css"
+  },
+  {
+    "revision": "ef68ca26791ec2af7a7d",
+    "url": "/react-BreakingBadQuotes/static/js/2.f931b20c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-BreakingBadQuotes/static/js/2.f931b20c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40e2564d498a6e6b5c5",
+    "url": "/react-BreakingBadQuotes/static/js/main.e776e96c.chunk.js"
+  },
+  {
+    "revision": "47d7d486491dc395f8a4",
+    "url": "/react-BreakingBadQuotes/static/js/runtime-main.72c246ad.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/react-BreakingBadQuotes/static/media/logo.427a4adc.svg"
+  }
+]);
